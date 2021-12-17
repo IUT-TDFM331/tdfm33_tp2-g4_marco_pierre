d3d1fe353521a69a8ef27ccbdf2d3b81d00956cf
marco <marco.laclavere@gmail.com>
2021-12-17 14:57:05 +0100

    suspend the error in SortAlgorithm

--- src/main/java/fr/unice/cdci/SortAlgorithms.java
+++ src/main/java/fr/unice/cdci/SortAlgorithms.java
@@ -17,6 +17,6 @@ public class SortAlgorithms {
         }
     }
 }
-    public void reverseSort(Integer[] values) {
-        // Cette méthode ne fait rien pour l'instant
-    }
+    // public void reverseSort(Integer[] values) {
+    //     // Cette méthode ne fait rien pour l'instant
+    // }
